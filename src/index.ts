d62055b1e43cf29ba398c4cf8ce3a877c0e67a58
serhankileci <serhanselimkileci@gmail.com>
2023-07-29 17:48:11 +0300

    separated middleware/route stack building logic

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import http from "node:http";
 import { Glacier, Request, Response, RequestMethod } from "./types.js";
-import { buildRoutingTable, response } from "./lib/index.js";
+import { buildRoutingTable, response, routeAndMiddlewareStack } from "./lib/index.js";
 
 const Glacier: Glacier = async options => {
 	console.log("❄️	Glacier.js is starting...");
 
 	const { port, routesDir } = options;
 
-	console.log("❄️	Building static routes...");
+	console.log("❄️	Building routes...");
 	const routingTable = await buildRoutingTable(routesDir);
 
 	http.createServer(async (httpReq, httpRes) => {
@@ -15,10 +15,10 @@ const Glacier: Glacier = async options => {
 			const chunks = [];
 			for await (const chunk of httpReq) chunks.push(chunk);
 			const body = JSON.parse(Buffer.concat(chunks).toString() || "null");
-
 			const url = new URL(httpReq.url || "/", `http://${httpReq.headers.host}`);
+			const { pathname } = url;
 
-			if (url.pathname === "/favicon.ico") {
+			if (pathname === "/favicon.ico") {
 				httpRes.statusCode = 404;
 				httpRes.end(404);
 
@@ -32,38 +32,10 @@ const Glacier: Glacier = async options => {
 				url,
 				params: {},
 				query: {},
-				booleans: {},
 			};
-			const res = response(httpRes, body);
+			const res = response(httpRes);
 
-			const separatedPaths = ["/"].concat(
-				url.pathname
-					.split("/")
-					.filter(Boolean)
-					.map(path => "/" + path)
-			);
-
-			const { before, main, after } = routingTable[url.pathname] || {};
-
-			for (const path of separatedPaths) {
-				const prev = routingTable[path];
-
-				if (prev?.before) {
-					await prev?.before(req, res);
-				}
-			}
-
-			if (before) await before(req, res);
-			await main(req, res);
-			if (after) await after(req, res);
-
-			for (const path of separatedPaths) {
-				const prev = routingTable[path];
-
-				if (prev?.after) {
-					await prev?.after(req, res);
-				}
-			}
+			await routeAndMiddlewareStack(pathname, routingTable, { req, res });
 		} catch (err: unknown) {
 			httpRes.end(JSON.stringify(err?.toString()));
 		}
