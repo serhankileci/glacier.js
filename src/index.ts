3808f0663ee725985197e3d1e08db25f941856d0
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-08 19:30:25 +0300

    moved the req obj logic to a func, misc.

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,37 @@
 import http from "node:http";
-import { Glacier, Request, Response, RequestMethod } from "./types.js";
+import { Glacier, GlacierRequest, GlacierResponse } from "./types.js";
 import {
 	buildRoutingTable,
-	defaultResponses,
-	parseParams,
-	parseRequestBody,
+	request,
 	response,
-	routeAndMiddlewareStack,
+	handleResponseAndMiddlewares,
+	defaultResponses,
+	normalizePathname,
 } from "./lib/index.js";
-import { normalizePathname } from "./lib/util.js";
 
 const Glacier: Glacier = async options => {
 	console.log("❄️	Glacier.js is starting...");
-
 	const { port, routesDir } = options;
-
 	console.log("❄️	Building routes...");
 	const routingTable = await buildRoutingTable(routesDir);
 
 	http.createServer(async (httpReq, httpRes) => {
 		try {
-			const normalizedPathname = normalizePathname(httpReq.url);
+			const normalizedPathname = normalizePathname(httpReq.url || "/");
 
 			if (normalizedPathname !== httpReq.url) {
-				httpRes.writeHead(302, {
-					Location: normalizedPathname,
-				});
-				httpRes.end();
+				httpRes.writeHead(302, { Location: normalizedPathname }).end();
 				return;
 			}
 
-			const method = httpReq.method?.toUpperCase() as RequestMethod;
-			const url = new URL(httpReq.url || "/", `http://${httpReq.headers.host}`);
-			const { pathname } = url;
+			const url = new URL(normalizedPathname, `http://${httpReq.headers.host}`);
 
-			if (pathname === "/favicon.ico") {
-				httpRes.statusCode = 404;
-				httpRes.end(404);
-				return;
-			}
+			const req = await request(httpReq, url, routingTable);
+			const res = await response(httpRes);
 
-			const req: Request = {
-				url,
-				method,
-				stdlib: httpReq,
-				query: Object.fromEntries(url.searchParams.entries()),
-				params: parseParams(routingTable, pathname),
-				body: await parseRequestBody(httpReq),
-			};
-			const res = response(httpRes);
-
-			await routeAndMiddlewareStack(pathname, routingTable, [req, res]);
+			await handleResponseAndMiddlewares(url.pathname, routingTable, [req, res]);
 		} catch (err: unknown) {
-			defaultResponses.internalServerError(JSON.stringify(err), httpReq, httpRes);
+			defaultResponses.internalServerError(err, httpReq, httpRes);
 		}
 	}).listen(port, () => {
 		console.log(`❄️	Glacier.js live on port ${port}.`);
@@ -60,4 +39,4 @@ const Glacier: Glacier = async options => {
 };
 
 export default Glacier;
-export type { Request, Response };
+export type { GlacierRequest, GlacierResponse };
