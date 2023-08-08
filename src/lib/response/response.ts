e9914d9f60fa527488f3ab87773cff35a9619a00
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-08 19:33:51 +0300

    replaced catch-all response method 'send' with specific data type methods like 'json'/'text'/'html'

--- src/lib/response/response.ts
+++ src/lib/response/response.ts
@@ -1,34 +1,51 @@
-import { ServerResponse } from "http";
-import { Response } from "../../types.js";
+import { ServerResponse } from "node:http";
+import { GlacierResponse } from "../../types.js";
+import { setHeaders } from "./util.js";
 
-function response(httpResponse: ServerResponse): Response {
+async function response(httpRes: ServerResponse): Promise<GlacierResponse> {
 	return {
-		stdlib: httpResponse,
+		stdlib: httpRes,
 		custom: {},
 		redirect: url => {
-			httpResponse.writeHead(302, { Location: url });
-			httpResponse.end();
+			httpRes.writeHead(302, { Location: url });
+			httpRes.end();
 		},
-		send: (data, options) => {
-			const { headers, status } = options || {};
-			if (status) httpResponse.statusCode = status;
+		json: (data, options) => {
+			const { status = 200, headers, customHeaders } = options || {};
+			httpRes.statusCode = status;
 
-			for (const [key, value] of Object.entries(headers || {}))
-				httpResponse.setHeader(key, value);
+			// override customHeaders in case they overlap with headers
+			setHeaders(httpRes, Object.assign({}, customHeaders, headers));
+			httpRes.setHeader("content-type", "application/json");
 
-			if (typeof data === "string" || typeof data === "number") {
-				if (headers && !headers["Content-Type"])
-					httpResponse.setHeader("Content-Type", "text/html");
-
-				data = String(data);
-			} else if (data instanceof Array || data instanceof Object) {
-				if (headers && !headers["Content-Type"])
-					httpResponse.setHeader("Content-Type", "application/json");
-
+			// stringify if not json
+			try {
+				if (data?.constructor === String) JSON.parse(data);
+			} catch (err: unknown) {
 				data = JSON.stringify(data);
 			}
 
-			httpResponse.end(data);
+			httpRes.end(data);
+		},
+		html: (data, options) => {
+			const { status = 200, headers, customHeaders } = options || {};
+			httpRes.statusCode = status;
+
+			// override customHeaders in case they overlap with headers
+			setHeaders(httpRes, Object.assign({}, customHeaders, headers));
+			httpRes.setHeader("content-type", "text/html");
+
+			httpRes.end(data);
+		},
+		text: (data, options) => {
+			const { status = 200, headers, customHeaders } = options || {};
+			httpRes.statusCode = status;
+
+			// override customHeaders in case they overlap with headers
+			setHeaders(httpRes, Object.assign({}, customHeaders, headers));
+			httpRes.setHeader("content-type", "text/plain");
+
+			httpRes.end(data?.toString());
 		},
 	};
 }
