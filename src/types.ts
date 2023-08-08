2eccc489236ee0f37bc59640665ee5aacac681d1
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-08 19:39:25 +0300

    renamed route handler 'main' -> 'handler', more dynamic req headers and content type types, misc. fixes, req cookies, specific res methods

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+import { IncomingHttpHeaders, IncomingMessage, ServerResponse } from "http";
+import { contentTypes, requestMethods } from "./lib/request/index.js";
+
 /**************************************************/
 type Glacier = (options: Configuration) => void;
 
@@ -6,64 +9,83 @@ type Configuration = {
 	routesDir: string;
 };
 
-type RouteHandler = (request: Request, response: Response) => Promise<void> | void;
+type RouteHandler = (request: GlacierRequest, response: GlacierResponse) => Promise<void> | void;
 
 type RoutingTable = {
 	[pathname: string]: {
 		before?: RouteHandler;
-		main: RouteHandler;
+		handler: RouteHandler;
 		after?: RouteHandler;
 	};
 };
 
-type RequestMethod =
-	| "GET"
-	| "POST"
-	| "PUT"
-	| "PATCH"
-	| "DELETE"
-	| "HEAD"
-	| "OPTIONS"
-	| "TRACE"
-	| "CONNECT";
+type RequestMethod = (typeof requestMethods)[number];
 
-type SharedSendArgs = {
-	status?: number;
-	headers?: Record<string, string>;
+type ContentType = (typeof contentTypes)[number];
+
+type RequestHeaders = {
+	[K in keyof IncomingHttpHeaders as string extends K
+		? never
+		: number extends K
+		? never
+		: K]: K extends "content-type" ? ContentType : IncomingHttpHeaders[K];
 };
 
-interface Request {
+type SharedSendArgs<T extends keyof RequestHeaders> = {
 	/**
-	 * Node.js http.Request. Use Glacier.Request instead unless necessary
+	 * default: 200 (OK)
 	 */
-	readonly stdlib: import("http").IncomingMessage;
+	status?: number;
+	headers?: Omit<RequestHeaders, T>;
+	customHeaders?: Record<string, string>;
+};
+
+interface GlacierRequest {
+	/**
+	 * node.js standard http.IncomingMessage
+	 */
+	readonly stdlib: IncomingMessage;
 	readonly method: RequestMethod;
 	readonly url: URL;
 	readonly body: Record<string, string>;
+	readonly cookies: IncomingHttpHeaders;
 	readonly query: Record<string, string>;
 	readonly params: Record<string, string>;
 }
 
-interface Response {
+interface GlacierResponse {
 	/**
-	 * node.js http.Response
-	 * use Glacier.Response instead unless necessary
+	 * node.js standard http.ServerResponse
 	 */
-	readonly stdlib: import("http").ServerResponse;
-	/**
-	 *
-	 * @param data raw data or path to file
-	 * @param options
-	 * @returns
-	 */
-	readonly send: (data: unknown, options?: SharedSendArgs) => void;
-	readonly redirect: (url: string) => void;
+	readonly stdlib: ServerResponse;
 	/**
 	 * custom object to store values in
 	 */
 	custom?: Record<string, unknown>;
+	readonly html: (data: string, options?: SharedSendArgs<"content-type">) => void;
+	readonly text: (data: TextData, options?: SharedSendArgs<"content-type">) => void;
+	readonly json: (data: JsonData, options?: SharedSendArgs<"content-type">) => void;
+	readonly redirect: (url: string) => void;
 }
 
+// misc.
+type TextData = string | number | boolean | null;
+type JsonData = string | number | boolean | null | { [key: string]: JsonData } | JsonData[];
+interface FStat {
+	fullPath: string;
+	isJsFile: boolean;
+	isDirectory: boolean;
+}
 /**************************************************/
 
-export { Glacier, Request, Response, RouteHandler, RequestMethod, RoutingTable };
+export {
+	Glacier,
+	GlacierRequest,
+	GlacierResponse,
+	RequestMethod,
+	RoutingTable,
+	RequestHeaders,
+	ContentType,
+	RouteHandler,
+	FStat,
+};
