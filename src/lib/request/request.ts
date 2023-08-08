ef02ccb96f932558b1d521ed1bf08f548d734940
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-08 19:36:12 +0300

    moved from app index to dedicated file

---
+++ src/lib/request/request.ts
@@ -0,0 +1,24 @@
+import { IncomingMessage } from "http";
+import { GlacierRequest, RequestMethod, RoutingTable } from "../../types.js";
+import { cookieParser, bodyParser, paramsParser } from "../middlewares/index.js";
+
+async function request(
+	httpReq: IncomingMessage,
+	url: URL,
+	routingTable: RoutingTable
+): Promise<GlacierRequest> {
+	const method = httpReq.method?.toUpperCase() as RequestMethod;
+	const { pathname } = url;
+
+	return {
+		url,
+		method,
+		stdlib: httpReq,
+		query: Object.fromEntries(url.searchParams.entries()),
+		cookies: cookieParser(httpReq.headers),
+		params: paramsParser(routingTable, pathname),
+		body: await bodyParser(httpReq),
+	};
+}
+
+export { request };
