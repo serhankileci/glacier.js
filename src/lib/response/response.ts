8c37c0554dea9f9825b2199e6b97ae33d999bb7a
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-11 23:58:45 +0300

    functionalized repetitive set status/header logic, added setCookie util method and xml response method

--- src/lib/response/response.ts
+++ src/lib/response/response.ts
@@ -1,22 +1,37 @@
 import { ServerResponse } from "node:http";
-import { GlacierResponse } from "../../types.js";
+import { ContentType, GlacierResponse, ResponseMethodOptions } from "../../types.js";
 import { setHeaders } from "./util.js";
+
+function setStatusAndHeaders(
+	httpRes: ServerResponse,
+	options: ResponseMethodOptions = {},
+	contentType: ContentType
+) {
+	const { status = 200, headers, customHeaders } = options;
+	httpRes.statusCode = status;
+
+	// override custom headers in case they overlap with standard headers
+	setHeaders(httpRes, Object.assign({}, customHeaders, headers));
+
+	// response method specific content type
+	httpRes.setHeader("content-type", contentType);
+}
 
 async function response(httpRes: ServerResponse): Promise<GlacierResponse> {
 	return {
-		stdlib: httpRes,
 		custom: {},
+		stdlib: httpRes,
+		util: {
+			setCookie: (key, value) => {
+				httpRes.setHeader(key, value);
+			},
+		},
 		redirect: url => {
 			httpRes.writeHead(302, { Location: url });
 			httpRes.end();
 		},
 		json: (data, options) => {
-			const { status = 200, headers, customHeaders } = options || {};
-			httpRes.statusCode = status;
-
-			// override customHeaders in case they overlap with headers
-			setHeaders(httpRes, Object.assign({}, customHeaders, headers));
-			httpRes.setHeader("content-type", "application/json");
+			setStatusAndHeaders(httpRes, options, "application/json");
 
 			// stringify if not json
 			try {
@@ -28,24 +43,16 @@ async function response(httpRes: ServerResponse): Promise<GlacierResponse> {
 			httpRes.end(data);
 		},
 		html: (data, options) => {
-			const { status = 200, headers, customHeaders } = options || {};
-			httpRes.statusCode = status;
-
-			// override customHeaders in case they overlap with headers
-			setHeaders(httpRes, Object.assign({}, customHeaders, headers));
-			httpRes.setHeader("content-type", "text/html");
-
+			setStatusAndHeaders(httpRes, options, "text/html");
 			httpRes.end(data);
 		},
 		text: (data, options) => {
-			const { status = 200, headers, customHeaders } = options || {};
-			httpRes.statusCode = status;
-
-			// override customHeaders in case they overlap with headers
-			setHeaders(httpRes, Object.assign({}, customHeaders, headers));
-			httpRes.setHeader("content-type", "text/plain");
-
+			setStatusAndHeaders(httpRes, options, "text/plain");
 			httpRes.end(data?.toString());
+		},
+		xml: (data, options) => {
+			setStatusAndHeaders(httpRes, options, "application/xml");
+			httpRes.end(data);
 		},
 	};
 }
