9ab4af7fdeb3400abe19619a6b26cf8d944d5d0d
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-11 23:51:54 +0300

    return undefined if no cookie header

--- src/lib/middlewares/cookieParser.ts
+++ src/lib/middlewares/cookieParser.ts
@@ -1,7 +1,7 @@
 import { IncomingHttpHeaders } from "http";
 
 function cookieParser(headers: IncomingHttpHeaders) {
-	if (!headers.cookie) return headers;
+	if (!headers.cookie) return undefined;
 
 	return headers.cookie.split(";").reduce<Record<string, string>>((prev, cur) => {
 		const [key, value] = cur.trim().split("=");
